71b2dde88190026aaeb6f1cade85bb56dd9c6bee
McKayla Washburn <mckayla@hey.com>
2023-07-13 23:08:41 -0600

    expose `TextPage`

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@ mod page;
 
 pub use page::html::HtmlPage;
 pub use page::md::MarkdownPage;
+pub use page::text::TextPage;
 pub use page::AsHtml;
 pub use page::OrderedPageCollection;
 pub use page::PageCollection;
